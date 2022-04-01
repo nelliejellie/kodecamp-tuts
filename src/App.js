e39b289544b0110b87dbad646328a516b3f1f119
Ewelike Emeka Nelson <emekaewelike@gmail.com>
2022-04-01 23:48:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
 
 function App() {
+  const [gitftcards, setGiftcards] = useState([])
+
+  useEffect(()=>{
+    getGiftCards()
+  },[])
+
+  const getGiftCards = async () =>{
+    const response = await fetch("https://nelliejellie.github.io/datajson/data/data.json")
+    const data = await response.json()
+    setGiftcards(data.allGiftCards)
+    console.log(data.allGiftCards)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App grid grid-cols-4 gap-4 bg-slate-400">
+      {gitftcards.map((item) => (
+        <div className='w-1/5 m-4' key={item.id}>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Title
+              </Typography>
+              <Typography variant="h5" component="div">
+                {item.title}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Currencies
+              </Typography>
+              <div className='grid grid-rows-2 gap-2 w-8'>
+                <span className='pl-2 text-blue-600'>{`${item.currencies[0]} `} </span>
+                <span className='pl-2 text-blue-600'>{`${item.currencies[1]} `} </span>
+                <span className='pl-2 text-blue-600'>{`${item.currencies[2]} `} </span>
+                <span className='pl-2 text-blue-600'>{`${item.currencies[3]} `} </span>
+                <span className='pl-2 text-blue-600'>{`${item.currencies[4]} `} </span>
+                <span className='pl-2 text-blue-600'>{`${item.currencies[5]} `} </span>
+                <span className='pl-2 text-blue-600'>{`${item.currencies[6]} `} </span>
+
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      ))}
+      
+      
     </div>
   );
 }
